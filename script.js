17d1c485acdb36e7261c7336772f62571d131e20
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-09-05 23:04:23 +0530

    embedding form

--- script.js
+++ script.js
@@ -10,158 +10,156 @@ window.addEventListener("load", () => {
     // document.getElementById('webcam').style.display = 'none';
     document.getElementById('timer').style.display = 'none';
     document.getElementById('fnn').style.display = 'none';
-    
+
     async function setupCamera() {
         video = document.getElementById('webcam');
         const stream = await navigator.mediaDevices.getUserMedia({ 'audio': false, 'video': true });
         video.srcObject = stream;
-    
+
         return new Promise((resolve) => {
             video.onloadedmetadata = () => {
                 resolve(video);
             };
         });
     }
-    
+
     async function loadDetector() {
         detector = await cocoSsd.load();
     }
-    
+
     async function detectHuman() {
         const predictions = await detector.detect(video);
         humanCount = predictions.filter(prediction => prediction.class === 'person').length;
         document.getElementById('fnn').innerText = `Number of humans detected: ${humanCount}`;
         return humanCount;
     }
-    
-     function startDetection() {
+
+    function startDetection() {
         return new Promise(async (resolve) => {
-        loadDetector().then(() => {
-            setupCamera().then(async (video) => {
-                video.play();
-                detectionInterval = setInterval(async () => {
-                    const humanCount = await detectHuman();
-                }, 1000); // Run detection every second
-                dtc = setInterval( function() {
-                    // resolve(humanCount);
-                    document.getElementById('timer').innerText = `Time remaining: ${dt} seconds`;
-                    dt--;
-                }, 1000);
-    
-                setTimeout(() => {
-                    clearInterval(detectionInterval);
-                    clearInterval(dtc);
-                    resolve(humanCount);
-                    video.srcObject.getTracks().forEach(track => track.stop()); // Stop video stream
-                    humanCount = document.getElementById('fnn').innerText.split(' ')[4];
-                    document.getElementById('webcam').style.display = 'none';
-                    document.getElementById('timer').style.display = 'none';
-                    document.getElementById('fnn').style.display = 'none';
-                }, detectionTime);
+            loadDetector().then(() => {
+                setupCamera().then(async (video) => {
+                    video.play();
+                    detectionInterval = setInterval(async () => {
+                        const humanCount = await detectHuman();
+                    }, 1000); // Run detection every second
+                    dtc = setInterval(function () {
+                        // resolve(humanCount);
+                        document.getElementById('timer').innerText = `Time remaining: ${dt} seconds`;
+                        dt--;
+                    }, 1000);
+
+                    setTimeout(() => {
+                        clearInterval(detectionInterval);
+                        clearInterval(dtc);
+                        resolve(humanCount);
+                        video.srcObject.getTracks().forEach(track => track.stop()); // Stop video stream
+                        humanCount = document.getElementById('fnn').innerText.split(' ')[4];
+                        document.getElementById('webcam').style.display = 'none';
+                        document.getElementById('timer').style.display = 'none';
+                        document.getElementById('fnn').style.display = 'none';
+                    }, detectionTime);
+                });
             });
         });
-    });
     }
 
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
+    const one = document.querySelector(".one");
+    const two = document.querySelector(".two");
+    const three = document.querySelector(".three");
+    const four = document.querySelector(".four");
 
 
-let one_selected = false
-let two_selected = false
-let three_selected = false
-let four_selected = false
+    let one_selected = false
+    let two_selected = false
+    let three_selected = false
+    let four_selected = false
 
-let cam_test_pass = false
+    let cam_test_pass = false
 
-one.onclick = function () {
-    one_selected = true
-    one.classList.add("active");
-    two.classList.remove("active");
-    three.classList.remove("active");
-    four.classList.remove("active");
-}
-
-two.onclick = function () {
-    if (one_selected) {
-        two_selected = true
+    one.onclick = function () {
+        one_selected = true
         one.classList.add("active");
-        two.classList.add("active");
+        two.classList.remove("active");
         three.classList.remove("active");
         four.classList.remove("active");
     }
-    else{
-    Swal.fire({
-        title: "Warning!",
-        text: "Kindly First confirm that you are in a safe place!",
-        icon: "warning",
-        confirmButtonText: "Okay",
-    });
-}
-}
-three.onclick = function () {
-    if (two_selected) {
 
-        x = Swal.fire({
-            title: "info",
-            text: "Kindly allow the camera permissions to access your camera!, We won't store any data!",
-            icon: "info",
-            showCancelButton: true,
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No'
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                three_selected = true;
-                one.classList.add("active");
-                two.classList.add("active");
-                three.classList.add("active");
-                four.classList.remove("active");
-                document.getElementById('timer').style.display = 'block';
-                document.getElementById('fnn').style.display = 'block';
-                x = await startDetection();
-                alert(x);
-                if (x > 1) {
-                    cam_test_pass = false;
-                    Swal.fire({
-                        title: "Error!",
-                        text: "More than one person detected! Try again!",
-                        icon: "error",
-                        confirmButtonText: "OK",
-                    });
-                }
-
-              
-            } else if (result.isDismissed === Swal.DismissReason.cancel) {
-              Swal.fire('You clicked No.', '', 'info');
-            }})
-    
+    two.onclick = function () {
+        if (one_selected) {
+            two_selected = true
+            one.classList.add("active");
+            two.classList.add("active");
+            three.classList.remove("active");
+            four.classList.remove("active");
+        }
+        else {
+            Swal.fire({
+                title: "Warning!",
+                text: "Kindly First confirm that you are in a safe place!",
+                icon: "warning",
+                confirmButtonText: "Okay",
+            });
+        }
     }
-    else{
-    Swal.fire({
-        title: "Error!",
-        text: "Kindly complete the previous steps!",
-        icon: "error",
-        confirmButtonText: "OK",
-    });
-}
+    three.onclick = function () {
+        if (two_selected) {
 
-    
-}
-// four.onclick = function () {
-//     if (three_selected) {
-//         four_selected = true
-//         one.classList.add("active");
-//         two.classList.add("active");
-//         three.classList.add("active");
-//         four.classList.add("active");
-//     }
-//     Swal.fire({
-//         title: "Hello!",
-//         text: "This is a SweetAlert pop-up!",
-//         icon: "info",
-//         confirmButtonText: "OK",
-//     });
-// }
+            x = Swal.fire({
+                title: "info",
+                text: "Kindly allow the camera permissions to access your camera!, We won't store any data!",
+                icon: "info",
+                showCancelButton: true,
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No'
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    three_selected = true;
+                    one.classList.add("active");
+                    two.classList.add("active");
+                    three.classList.add("active");
+                    four.classList.remove("active");
+                    document.getElementById('timer').style.display = 'block';
+                    document.getElementById('fnn').style.display = 'block';
+                    x = await startDetection();
+                    // alert(x);
+                    if (x > 1) {
+                        cam_test_pass = false;
+                        Swal.fire({
+                            title: "Error!",
+                            text: "More than one person detected! Try again!",
+                            icon: "error",
+                            confirmButtonText: "OK",
+                        });
+                    }
+                    else {
+                        cam_test_pass = true;
+                        one.classList.add("active");
+                        two.classList.add("active");
+                        three.classList.add("active");
+                        four.classList.add("active");
+
+
+                        document.getElementById('options').style.display = 'none';
+                        document.getElementById('result').innerHTML = `
+                                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfY8J49KTT4ASEtgAQa5tTCiQg_WAVYPLMVc5auPy6voEZvmA/viewform?embedded=true" width="640" height="425" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                            `;
+
+
+                    }
+
+                } else if (result.isDismissed === Swal.DismissReason.cancel) {
+                    Swal.fire('You clicked No.', '', 'info');
+                }
+            })
+
+        }
+        else {
+            Swal.fire({
+                title: "Error!",
+                text: "Kindly complete the previous steps!",
+                icon: "error",
+                confirmButtonText: "OK",
+            });
+        }
+    }
 });
